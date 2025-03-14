98b70bc788bdb8e70b526fb0523cdbd2eaf6b867
unknown <koudankomoise@gmail.com>
2025-03-14 11:39:49 +0100

    Ajout des pages des authentifications

--- mediclic/lib/main.dart
+++ mediclic/lib/main.dart
@@ -1,8 +1,8 @@
 // main.dart
 import 'package:firebase_core/firebase_core.dart';
+import 'package:mediclic/pages/connexionpages/login_screen.dart';
 
 import 'firebase_options.dart';
-import 'pages/navigation.dart';
 import 'package:flutter/material.dart';
 
 // ../mediclinique/lib/main.dart
@@ -17,6 +17,6 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(debugShowCheckedModeBanner: false, home: MyAppHome());
+    return MaterialApp(debugShowCheckedModeBanner: false, home: LoginScreen());
   }
 }
